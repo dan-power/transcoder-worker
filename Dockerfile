63ca4b81abf7d49cd39c69872d574e1df4984cfd
Dan Power <dan-power@users.noreply.github.com>
2017-08-19 22:57:09 +1000

    handbrake-js works better

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,18 @@ FROM node
 
 MAINTAINER Dan Power
 ENV RABBITMQ=rabbitmq
+ENV WATCH_PATH="/mnt/media"
 
 LABEL project="au.id.danpower.transcoder"
 LABEL version=1.0
 
-RUN apt-get update && apt-get install -y git
-RUN git clone https://github.com/dan-power/transcoder-worker.git /var/transcoder-worker
+VOLUME $WATCH_PATH
+
+RUN add-apt-repository --yes ppa:stebbins/handbrake-releases && \
+    apt-get update && \
+    apt-get install -y git && \
+    apt-get install -qq handbrake-cli && \
+    git clone https://github.com/dan-power/transcoder-worker.git /var/transcoder-worker
 WORKDIR /var/transcoder-worker
 RUN npm install
 
--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
-#!/usr/bin/env node
 // Process tasks from the work queue
-
 var amqp = require('amqplib');
+var Duration = require('duration')
+var hbjs = require('handbrake-js')
 
 amqp.connect('amqp://'+process.env.RABBITMQ).then(function(conn) {
   process.once('SIGINT', function() { conn.close(); });
@@ -16,13 +16,34 @@ amqp.connect('amqp://'+process.env.RABBITMQ).then(function(conn) {
 
     function doWork(msg) {
       var body = msg.content.toString();
-      console.log(" [x] Received '%s'", body);
-      var secs = body.split('.').length - 1;
-      console.log(" [x] Task takes %d seconds", secs);
-      setTimeout(function() {
-        console.log(" [x] Done");
+      console.log(" [x] Received '%s'", body.filename);
+
+      // { filename: file, options: options }
+      const fnLen = body.filename.length;
+
+      // Dont like this, but it will do for now.
+      const fnExt = body.filename.substr(fnLen - 3,3);
+      const output = body.filename.replace(fnExt, 'mp4');
+      const start = new Date();
+
+      body.options.input = body.filename;
+      body.options.output = output;
+
+      hbjs.spawn(body.options)
+      .on('error', function(err){
+        // invalid user input, no video found etc
+        console.error("%s: %s", err.errno, err.message);
+      })
+      .on('progress', function(progress){
+        console.log('Percent complete: %s, ETA: %s', progress.percentComplete, progress.eta);
+      })
+      .on('end', function() {
+        const finish = new Date();
+        var duration = new Duration(start, finish);
+
+        console.log(" [x] Converted to %s in %s", output, duration.toString("H: %Hs m: %M"));
         ch.ack(msg);
-      }, secs * 1000);
+      });
     }
   });
 }).catch(console.warn);
